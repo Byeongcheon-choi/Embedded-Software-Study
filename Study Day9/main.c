595a2e915a03d66ec796287a2cce96454c763389
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-10-30 15:44:39 -0400

    Update main.c

--- Study Day9/main.c
+++ Study Day9/main.c
@@ -1,6 +1,6 @@
 # include "stm32f3xx.h" 
 
-#define PROI_TIM3 3
+#define PROI_TIM3 3 /* set priority */
 #define PROI_TIM4 4
 
 
@@ -9,32 +9,32 @@ void DELAYMODE(int a);
 int main(void)
 {
 	__disable_irq();
-	RCC->AHBENR |= 0X00020000;
-	RCC->AHBENR |= 0X00040000;
+	RCC->AHBENR |= 0X00020000; /*enable GPIOA clock */
+	RCC->AHBENR |= 0X00040000; /*enable GPIOB clock */
 	
-	GPIOA->MODER &= ~0X00000C00;
-	GPIOA->MODER |=  0X00000400;
-	GPIOA->MODER &= ~0X000003C0;
-	GPIOA->MODER &=  0X00000140;
+	GPIOA->MODER &= ~0X00000C00; /* clear PA5 */
+	GPIOA->MODER |=  0X00000400; /* Set PA5 output*/
+	GPIOA->MODER &= ~0X000003C0; /* clear PB3,4 */
+	GPIOA->MODER &=  0X00000140; /* Set PB3,4 output */
 	
-	RCC->APB1ENR |=  0X00000020;
-	RCC->APB1ENR |=  0X00000040;
+	RCC->APB1ENR |=  0X00000020; /*TIM3 enable */
+	RCC->APB1ENR |=  0X00000040; /*TIM4 enable */
 	
-	TIM3->PSC = 1000-1;
+	TIM3->PSC = 1000-1; /*TIM3 setting */
 	TIM3->ARR = 15500-1;
 	TIM3->CR1 = 1;
 	TIM3->DIER = 1;
 	
-	TIM4->PSC = 1000-1;
+	TIM4->PSC = 1000-1; /*TIM4 setting */
 	TIM4->ARR = 1562-1;
 	TIM4->CR1 = 1;
 	TIM4->DIER = 1;
 	
-	NVIC_SetPriority(TIM3_IRQn,PROI_TIM3);
-	NVIC_EnableIRQ(TIM3_IRQn);
+	NVIC_SetPriority(TIM3_IRQn,PROI_TIM3); /* set priority*/
+	NVIC_EnableIRQ(TIM3_IRQn); /*trigger Interrupt */
 	
-	NVIC_SetPriority(TIM4_IRQn,PROI_TIM4);
-	NVIC_EnableIRQ(TIM4_IRQn);
+	NVIC_SetPriority(TIM4_IRQn,PROI_TIM4); /* set priority */
+	NVIC_EnableIRQ(TIM4_IRQn); /*trigger Interrupt */
 	
 	__enable_irq();
 	
@@ -43,7 +43,7 @@ int main(void)
 	
 }
 
-void TIM3_IRQHandler(void){
+void TIM3_IRQHandler(void){ /* In the case of triggerring TIM3 */
 	GPIOB->BSRR |= 0X00000008;
 	DELAYMODE(500);
 	GPIOB->BSRR |= 0X00000008 <<16;
@@ -51,7 +51,7 @@ void TIM3_IRQHandler(void){
 	TIM3->SR=0;
 }
 
-void TIM4_IQRHandler(void){
+void TIM4_IQRHandler(void){ /* In the case of triggering TIM4, but TIM3 has higher priority, So this one is not operating. */
 	GPIOA->BSRR |= 0X00000020;
 	GPIOA->BSRR |= 0X00000020;
 	DELAYMODE(25);
