d269ca039dffe4b6db86743b992b4faed380b0d9
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-11-08 19:21:42 -0500

    Update SPI.c

--- Study day12/SPI.c
+++ Study day12/SPI.c
@@ -10,36 +10,36 @@ int main(void){
 	
 	while(1){
 		for(char i = 'A';i <= 'Z'; i++){
-			SPI_Start(i);
+			SPI_Start(i); /* write the letter */
 			Delaymode(100);
 		}
 	}
 }
 
 void SPI_Init(void){
-	RCC->AHBENR |= 1 << 17;
-	RCC->APB2ENR |= 1 << 12;
+	RCC->AHBENR |= 1 << 17; /*GPIOA clock enable */
+	RCC->APB2ENR |= 1 << 12; /* SPI clock enalbe*/
 	
-	GPIOA->MODER &= ~0X0000CC00;
-	GPIOA->MODER |= 0X00008800;
-	GPIOA->AFR[0] &= ~0XF0F00000;
-	GPIOA->AFR[0] |= 0X50500000;
+	GPIOA->MODER &= ~0X0000CC00; /*clear */
+	GPIOA->MODER |= 0X00008800; /*set PA5,7 to alternate mode */
+	GPIOA->AFR[0] &= ~0XF0F00000; /* clear*/
+	GPIOA->AFR[0] |= 0X50500000; /*set AF5 */
 	
-	GPIOA->MODER &= ~0X00000300;
-	GPIOA->MODER |= 0X00000100;
+	GPIOA->MODER &= ~0X00000300; /* clear */
+	GPIOA->MODER |= 0X00000100; /* set PA4 Output mode */
 	
-	SPI1->CR1 |= 0X031C;
-	SPI1->CR2 = 0;
-	SPI1->CR1 |= 0x0040;
+	SPI1->CR1 |= 0X031C; /*set SPI */
+	SPI1->CR2 = 0; 
+	SPI1->CR1 |= 0x0040; /* SPI enable */
 }
 
 void SPI_Start(unsigned char data)
 {
-	while(!(SPI1->SR & 0X0002)){}
-	GPIOA->BSRR |= 0X00000010;
-	SPI1->DR = data;
-	while(SPI1->SR & 0X0080){}
-	GPIOA->BSRR |= 0X00000010<<16;
+	while(!(SPI1->SR & 0X0002)); /* wait the transfer buffer empty*/
+	GPIOA->BSRR |= 0X00000010<<16; /*Reset the Pin status*/
+	SPI1->DR = data; /*write data */
+	while(SPI1->SR & 0X0080); /*Wait until transfering data is finished */
+	GPIOA->BSRR |= 0X00000010; /*Set the Pin status*/
 }
 
 void Delaymode(int n){
